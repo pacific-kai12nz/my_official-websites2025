85b9212471212c5bd52621b25da82c55ac055e11
kaipora12 <kaipora_12nz@icloud.com>
2025-06-15 00:29:06 +0900

    commit init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 import i18next from "astro-i18next";
 
 export default defineConfig({
   integrations: [
     react(),
+    tailwind(),
     i18next({
       defaultLocale: "ja",
       locales: ["ja", "en"],
+      i18nextOptions: { fallbackLng: "ja" },
     }),
   ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
