bd718a5aac2c1ee6b808e861263c27ff3a26e8b1
kaipora12 <kaipora_12nz@icloud.com>
2026-02-18 00:37:14 +0900

    commit init

---
+++ src/components/ReactCarousel.jsx
@@ -0,0 +1,42 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const works = [
+  { src: "/images/comigsoon.jpg", title: "WORK1", link: "" },
+  { src: "/images/comigsoon.jpg", title: "WORK2", link: "" },
+  { src: "/images/comigsoon.jpg", title: "WORK3", link: "" },
+  { src: "/images/comigsoon.jpg", title: "WORK4", link: "" },
+];
+
+const Carousel = () => {
+  return (
+    <div className="carousel-container">
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={3}
+        loop={true}
+        loopAdditionalSlides={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        breakpoints={{
+          480: { slidesPerView: 1, spaceBetween: 10 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }}
+      >
+        {works.map((work, index) => (
+          <SwiperSlide key={index}>
+            <a href={work.link}>
+              <img src={work.src} alt={work.title} className="carousel-image" />
+            </a>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Carousel;
